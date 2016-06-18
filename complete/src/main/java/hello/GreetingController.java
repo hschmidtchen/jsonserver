8451bb020aeba9de445137c28fbd5b4cd80ba94a
Tobias Duemmling <duemmlingcip.ifi.lmu.de>
2016-06-18 18:45:38 +0200

    Added another request option

--- complete/src/main/java/hello/GreetingController.java
+++ complete/src/main/java/hello/GreetingController.java
@@ -7,13 +7,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+	private final String matlabRequets = "matlabUpdate";
+	private boolean readyForUpdate = true;
+	
+	
 
     private static final String template = "Hello, %s!";
+
+
+
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
         return new Greeting(counter.incrementAndGet(),
                             String.format(template, name));
+    }
+    
+    @RequestMapping("/matlabUpdate")
+    public String MatlabUpdate(){
+    	 return "Tobi ist super";
     }
 }
--- complete/src/test/java/hello/GreetingControllerTests.java
+++ complete/src/test/java/hello/GreetingControllerTests.java
@@ -14,7 +14,6 @@
  * limitations under the License.
  */
 package hello;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
